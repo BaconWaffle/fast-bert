54996b7f596b94ca53714b94155500a9de514b71
BaconWaffle <BaconWaffle@users.noreply.github.com>
2019-08-27 13:53:14 +0200

    Update data_cls.py

--- fast_bert/data_cls.py
+++ fast_bert/data_cls.py
@@ -99,6 +99,8 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
         if ex_index % 10000 == 0:
             if logger:
                 logger.info("Writing example %d of %d" % (ex_index, len(examples)))
+                
+        print(type(example.text_a))
         tokens_a = tokenizer.tokenize(text=example.text_a)
 
         tokens_b = None
